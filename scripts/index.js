df04f4d8753b5a13a549b8854500220e6903f984
Julia <july912655@gmail.com>
2023-03-06 10:24:18 +0500

    rename class

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
-const popupEditButtonElement = document.querySelector('.profile__button_edit');
+const popupEditButtonElement = document.querySelector('.profile__button-edit');
 const popupSaveButtonElement = popupElement.querySelector('.popup__save');
 
 const addPopup = function addPopup() {
@@ -19,7 +19,7 @@ let nameInput = formElement.querySelector('.popup__name');
 let descriptionInput = formElement.querySelector('.popup__description');
 
 function handleFormSubmit (evt) {
-     evt.preventDefault(); 
+     evt.preventDefault();
     let nameProfile = document.querySelector('.profile__name');
     let descriptionProfile = document.querySelector('.profile__description');
     nameProfile.textContent = nameInput.value;
@@ -29,9 +29,8 @@ function handleFormSubmit (evt) {
 
 popupSaveButtonElement.addEventListener("click", formElement.addEventListener('submit', handleFormSubmit));
 
- 
 
 
 
 
- 
+
