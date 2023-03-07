171149959583d6de5e387472021759d1d47232af
Julia <july912655@gmail.com>
2023-03-07 05:30:19 +0500

    fix bugs in the whole project

--- scripts/index.js
+++ scripts/index.js
@@ -2,32 +2,30 @@ const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 const popupEditButtonElement = document.querySelector('.profile__button-edit');
 const popupSaveButtonElement = popupElement.querySelector('.popup__save');
+let nameProfile = document.querySelector('.profile__name');
+let descriptionProfile = document.querySelector('.profile__description');
+let formElement = document.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__input_type_name');
+let descriptionInput = formElement.querySelector('.popup__input_type_aboutme');
 
 const addPopup = function addPopup() {
-  popupElement.classList.add('popup_is-opened');
+  popupElement.classList.add('popup_opened');
 }
 
 const closePopup = function closePopup() {
-  popupElement.classList.remove('popup_is-opened');
+  popupElement.classList.remove('popup_opened');
+}
+
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+ nameProfile.textContent = nameInput.value;
+ descriptionProfile.textContent = descriptionInput.value;
+ closePopup();
 }
 
 popupEditButtonElement.addEventListener("click", addPopup);
 popupCloseButtonElement.addEventListener("click", closePopup);
-
-let formElement = document.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.popup__name');
-let descriptionInput = formElement.querySelector('.popup__description');
-
-function handleFormSubmit (evt) {
-     evt.preventDefault();
-    let nameProfile = document.querySelector('.profile__name');
-    let descriptionProfile = document.querySelector('.profile__description');
-    nameProfile.textContent = nameInput.value;
-    descriptionProfile.textContent = descriptionInput.value;
-    closePopup();
-}
-
-popupSaveButtonElement.addEventListener("click", formElement.addEventListener('submit', handleFormSubmit));
+formElement.addEventListener('submit', handleFormSubmit);
 
 
 
