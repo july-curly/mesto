51cae1865d3963a4f70a0d6973670543c73f5534
Julia <july912655@gmail.com>
2023-04-06 18:48:14 +0500

    добавление полей ошибки

--- scripts/validate.js
+++ scripts/validate.js
@@ -3,44 +3,58 @@ const validationConfig = {
   inputSelector: '.popup__input',
   submitButtonSelector: '.popup__save',
   inactiveButtonClass: 'popup__save_disabled',
-  inputErrorClass: 'popup__input-error',
-  // errorClass: 'popup__error_visible'
+  inputErrorClass: 'popup__input_error',
+  errorClass: 'popup__error'
 }
 
 const enableValidation = (config) => {
   const forms = Array.from(config.formSelector);
-   forms.forEach(form => {
-    const formInputs = form.querySelectorAll(config.inputSelector);
-    const buttonElement = form.querySelector(config.submitButtonSelector);
-    console.log(formInputs, buttonElement);
+    forms.forEach(form => {
+      const formInputs = form.querySelectorAll(config.inputSelector);
+      const buttonElement = form.querySelector(config.submitButtonSelector);
   //   form.addEventListener('submit', (evt) => {
   //     evt.preventDefault();
   //   })
-  //  // setEventListener(form);
-   })
+    putEventListeners(formInputs, buttonElement, config.inactiveButtonClass, config.inputErrorClass, config.errorClass);
+    })
 
 }
 
-//const setEventListener = (formToValidate) => {
+const putEventListeners = (formInputs, buttonElement, inactiveButtonClass, inputErrorClass, errorClass) => {
  // const formInputs = Array.from(formToValidate.inputSelector);
 //  const buttonElement = formToValidate.querySelector(submitButtonSelector) ;
-//   formInputs.forEach(input => {
-//     input.addEventListener('input', () => {
-//       checkInputValidity(input);
+   formInputs.forEach(input => {
+      input.addEventListener('input', () => {
+      checkInputValidity(input, inputErrorClass, errorClass);
+     // toggleButtonState(input, buttonElement, inactiveButtonClass);
 
-//     } )
-//   })
-//}
+     } )
+   })
+}
 
-// const checkInputValidity = (input) => {
-//   const currentInputErrorContainer = document.querySelector(`#${input.id}-error`)
-//   if (input.checkValidity) {
-//     currentInputErrorContainer.textContent = '';
-//   }
-//   else{
-//     currentInputErrorContainer.textContent = input.validationMessage;
-//   }
-// }
+const checkInputValidity = (input, inputErrorClass, errorClass) => {
+  const currentInputErrorContainer = document.querySelector(`#${input.id}-error`)
+  if (input.validity.valid) {
+    console.log("ok");
+    hideInputError(input, currentInputErrorContainer, inputErrorClass, errorClass);
+  }
+  else {
+    console.log("fuck off");
+    showInputError(input, currentInputErrorContainer, inputErrorClass, errorClass);
+  }
+}
+
+const showInputError = (input, currentInputErrorContainer, inputErrorClass, errorClass) => {
+  input.classList.add(inputErrorClass);
+  currentInputErrorContainer.textContent = input.validationMessage;
+  currentInputErrorContainer.classList.add(errorClass);
+};
+
+const hideInputError = (input, currentInputErrorContainer, inputErrorClass, errorClass) => {
+  input.classList.remove(inputErrorClass);
+  currentInputErrorContainer.classList.remove(errorClass);
+  currentInputErrorContainer.textContent = '';
+};
 
 enableValidation(validationConfig);
 
